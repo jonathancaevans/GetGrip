9f54c0847460efd21342f6de953ccb347628f1fb
Kunhunjon <jonathan.ca.evans@gmail.com>
2020-07-17 14:10:24 -0400

    Added set stats chart update function, now runs on page load for initial data and adding of new form element

--- app/javascript/packs/custom.js
+++ app/javascript/packs/custom.js
@@ -1,84 +1,12 @@
 $(document).on('turbolinks:load', function() {
   //Color set ajax update
   $('body').on('select2:select', function (e) {
-    var colors = new Array();
-    var grades = new Array();
-
-    var colorCounter = 0;
-    var gradeCounter = 0;
-
-    $('.color').find(':selected').each(function( index ) {
-      var i;
-      var newColor = true;
-
-      for(i = 0; i < colors.length; i++){
-        if(colors[i].name == $(this).text())
-        {
-          var data = new Object();
-          data["Occurance"] = colors[i].data["Occurance"] + 1;
-
-          colors[i].data = data;
-          i = colors.length;
-          newColor = false;
-        }
-      }
-
-      if(newColor)
-      {
-        var color = new Object();
-        color.name = $(this).text();
-
-        var data = new Object();
-        data["Occurance"] = 1;
-        color.data = data;
-
-        color.color = $(this).attr('data-swatch');
-
-        colors[colorCounter] = color;
-
-        colorCounter++;
-      }
-    });
-
-    $('.grade').find(':selected').sort((a,b) => $(a).data("difficulty") - $(b).data("difficulty")).each(function( index ) {
-      var i;
-      var newGrade = true;
-
-      for(i = 0; i < grades.length; i++){
-        if(grades[i].name == $(this).text())
-        {
-          var data = new Object();
-          data["Occurance"] = grades[i].data["Occurance"] + 1;
-
-          grades[i].data = data;
-          i = grades.length;
-          newGrade = false;
-        }
-      }
-
-      if(newGrade)
-      {
-        var grade = new Object();
-        grade.name = $(this).text();
-
-        var data = new Object();
-        data["Occurance"] = 1;
-        grade.data = data;
-
-        grades[gradeCounter] = grade;
-
-        gradeCounter++;
-      }
-    });
-
-    var chart = Chartkick.charts["chart-1"];
-    var chart2 = Chartkick.charts["chart-2"];
-    chart.updateData(colors)
-    chart2.updateData(grades)
+    updateStats();
   });
 
   $(document).ready(function() {
     $('select').select2();
+    updateStats();
   });
 
   $('form').on('click', '.remove_record', function(event) {
@@ -93,6 +21,7 @@ $(document).on('turbolinks:load', function() {
     regexp = new RegExp($(this).data('id'), 'g');
     $('.fields').append($(this).data('fields').replace(regexp, time));
     $('select').select2();
+    updateStats();
     return event.preventDefault();
   });
   
@@ -105,3 +34,81 @@ $(document).on('turbolinks:load', function() {
     }
   });
 });
+
+function updateStats()
+{
+  var colors = new Array();
+  var grades = new Array();
+
+  var colorCounter = 0;
+  var gradeCounter = 0;
+
+  $('.color').find(':selected').each(function( index ) {
+    var i;
+    var newColor = true;
+
+    for(i = 0; i < colors.length; i++){
+      if(colors[i].name == $(this).text())
+      {
+        var data = new Object();
+        data["Occurance"] = colors[i].data["Occurance"] + 1;
+
+        colors[i].data = data;
+        i = colors.length;
+        newColor = false;
+      }
+    }
+
+    if(newColor)
+    {
+      var color = new Object();
+      color.name = $(this).text();
+
+      var data = new Object();
+      data["Occurance"] = 1;
+      color.data = data;
+
+      color.color = $(this).attr('data-swatch');
+
+      colors[colorCounter] = color;
+
+      colorCounter++;
+    }
+  });
+
+  $('.grade').find(':selected').sort((a,b) => $(a).data("difficulty") - $(b).data("difficulty")).each(function( index ) {
+    var i;
+    var newGrade = true;
+
+    for(i = 0; i < grades.length; i++){
+      if(grades[i].name == $(this).text())
+      {
+        var data = new Object();
+        data["Occurance"] = grades[i].data["Occurance"] + 1;
+
+        grades[i].data = data;
+        i = grades.length;
+        newGrade = false;
+      }
+    }
+
+    if(newGrade)
+    {
+      var grade = new Object();
+      grade.name = $(this).text();
+
+      var data = new Object();
+      data["Occurance"] = 1;
+      grade.data = data;
+
+      grades[gradeCounter] = grade;
+
+      gradeCounter++;
+    }
+  });
+
+  var chart = Chartkick.charts["chart-1"];
+  var chart2 = Chartkick.charts["chart-2"];
+  chart.updateData(colors);
+  chart2.updateData(grades);
+}
