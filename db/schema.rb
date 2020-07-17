6f5392e6af7bf21f87c1f6ae6bac499b6b62290c
Kunhunjon <jonathan.ca.evans@gmail.com>
2020-07-17 14:26:43 -0400

    Added username to devise users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_29_022018) do
+ActiveRecord::Schema.define(version: 2020_07_17_182110) do
 
   create_table "companies", force: :cascade do |t|
     t.string "name"
@@ -103,8 +103,10 @@ ActiveRecord::Schema.define(version: 2020_06_29_022018) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
   create_table "walls", force: :cascade do |t|
