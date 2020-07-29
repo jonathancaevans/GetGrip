38c2ceb7d1eaf62a8f51b9a0c4fb0660874eb634
Kunhunjon <jonathan.ca.evans@gmail.com>
2020-07-29 15:21:52 -0400

    Attempted to fix gitignore in hopes server will run webpacker

---
+++ public/packs/js/custom-f727feeaae88aa21fda1.js
@@ -0,0 +1,101 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "/packs/";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = "./app/javascript/packs/custom.js");
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./app/javascript/packs/custom.js":
+/*!****************************************!*\
+  !*** ./app/javascript/packs/custom.js ***!
+  \****************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+throw new Error("Module build failed (from ./node_modules/babel-loader/lib/index.js):\nSyntaxError: /home/getgrip/Desktop/GetGrip/app/javascript/packs/custom.js: Unexpected token, expected \",\" (49:1)\n\n  47 |     }\n  48 |   });\n> 49 | }\n     |  ^\n    at Parser._raise (/home/getgrip/Desktop/GetGrip/node_modules/@babel/parser/lib/index.js:742:17)\n    at Parser.raiseWithData (/home/getgrip/Desktop/GetGrip/node_modules/@babel/parser/lib/index.js:735:17)\n    at Parser.raise (/home/getgrip/Desktop/GetGrip/node_modules/@babel/parser/lib/index.js:729:17)\n    at Parser.unexpected (/home/getgrip/Desktop/GetGrip/node_modules/@babel/parser/lib/index.js:8779:16)\n    at Parser.expect (/home/getgrip/Desktop/GetGrip/node_modules/@babel/parser/lib/index.js:8765:28)\n    at Parser.parseCallExpressionArguments (/home/getgrip/Desktop/GetGrip/node_modules/@babel/parser/lib/index.js:9792:14)\n    at Parser.parseSubscript (/home/getgrip/Desktop/GetGrip/node_modules/@babel/parser/lib/index.js:9718:31)\n    at Parser.parseSubscripts (/home/getgrip/Desktop/GetGrip/node_modules/@babel/parser/lib/index.js:9647:19)\n    at Parser.parseExprSubscripts (/home/getgrip/Desktop/GetGrip/node_modules/@babel/parser/lib/index.js:9630:17)\n    at Parser.parseMaybeUnary (/home/getgrip/Desktop/GetGrip/node_modules/@babel/parser/lib/index.js:9604:21)\n    at Parser.parseExprOps (/home/getgrip/Desktop/GetGrip/node_modules/@babel/parser/lib/index.js:9474:23)\n    at Parser.parseMaybeConditional (/home/getgrip/Desktop/GetGrip/node_modules/@babel/parser/lib/index.js:9447:23)\n    at Parser.parseMaybeAssign (/home/getgrip/Desktop/GetGrip/node_modules/@babel/parser/lib/index.js:9402:21)\n    at Parser.parseExpression (/home/getgrip/Desktop/GetGrip/node_modules/@babel/parser/lib/index.js:9354:23)\n    at Parser.parseStatementContent (/home/getgrip/Desktop/GetGrip/node_modules/@babel/parser/lib/index.js:11233:23)\n    at Parser.parseStatement (/home/getgrip/Desktop/GetGrip/node_modules/@babel/parser/lib/index.js:11104:17)\n    at Parser.parseBlockOrModuleBlockBody (/home/getgrip/Desktop/GetGrip/node_modules/@babel/parser/lib/index.js:11679:25)\n    at Parser.parseBlockBody (/home/getgrip/Desktop/GetGrip/node_modules/@babel/parser/lib/index.js:11665:10)\n    at Parser.parseTopLevel (/home/getgrip/Desktop/GetGrip/node_modules/@babel/parser/lib/index.js:11035:10)\n    at Parser.parse (/home/getgrip/Desktop/GetGrip/node_modules/@babel/parser/lib/index.js:12671:10)\n    at parse (/home/getgrip/Desktop/GetGrip/node_modules/@babel/parser/lib/index.js:12722:38)\n    at parser (/home/getgrip/Desktop/GetGrip/node_modules/@babel/core/lib/parser/index.js:54:34)\n    at parser.next (<anonymous>)\n    at normalizeFile (/home/getgrip/Desktop/GetGrip/node_modules/@babel/core/lib/transformation/normalize-file.js:93:38)\n    at normalizeFile.next (<anonymous>)\n    at run (/home/getgrip/Desktop/GetGrip/node_modules/@babel/core/lib/transformation/index.js:31:50)\n    at run.next (<anonymous>)\n    at Function.transform (/home/getgrip/Desktop/GetGrip/node_modules/@babel/core/lib/transform.js:27:41)\n    at transform.next (<anonymous>)\n    at step (/home/getgrip/Desktop/GetGrip/node_modules/gensync/index.js:254:32)");
+
+/***/ })
+
+/******/ });
+//# sourceMappingURL=custom-f727feeaae88aa21fda1.js.map
